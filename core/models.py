4ff71ddf7f10558c132045a12e4d0e6a0e1b5902
MoochontheMic <lachlanharding2000@gmail.com>
2022-04-06 18:25:26 +0100

    board generating and inital paths from the logic established

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+MAX_HEIGHT = 3
+MAX_WIDTH = 3
 # Create your models here.
+class Board():
+    
+    def __init__(self):
+        """
+        initalises array of board
+        """
+        self.name = 'test'
+        self.array = [[' ' for item in range(MAX_HEIGHT)] for other in range(MAX_WIDTH)]
--- core/views.py
+++ core/views.py
@@ -4,10 +4,11 @@
 
 from django.shortcuts import render
 from django.views import View
+import core.models
 
+board = core.models.Board()
 
 class Index(View):
     template = 'index.html'
-
     def get(self, request):
-        return render(request, self.template)
+        return render(request, self.template, {'board': board})
